66eaa20bb48b9a3c7d5e79dc00007d572972d95c
Kajamohan <2018e055@eng.jfn.ac.lk>
2023-07-04 21:59:13 +0530

    basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,29 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './layout/login/login.component';
 import { LoadingComponent } from './helpers/loading/loading.component';
 import { SharedModule } from './shared/shared.module';
-import { DashboardModule } from './layout/dashboard/dashboard.module';
 import { HomeComponent } from './layout/components/student/home/home.component';
 import { SubjectsComponent } from './layout/components/student/subjects/subjects.component';
-import { StudentComponent } from './layout/components/student/student.component';
 import { AdminComponent } from './layout/components/admin/admin.component';
 import { TeacherComponent } from './layout/components/teacher/teacher.component';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
+import { SidebarComponent } from './layout/dashboard/sidebar/sidebar.component';
+import { HeaderComponent } from './layout/dashboard/header/header.component';
+import { FooterComponent } from './layout/dashboard/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, LoadingComponent, HomeComponent, SubjectsComponent, StudentComponent, AdminComponent, TeacherComponent],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    SidebarComponent,
+    LoginComponent,
+    LoadingComponent,
+    HomeComponent,
+    SubjectsComponent,
+    AdminComponent,
+    TeacherComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
